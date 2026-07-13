c4ad35c1cac8f735549943de490a4acd6f8aab33
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-07-13 14:36:07 +0800

    Revert "Create Sidebar and Input field."
    This reverts commit a1bc4a3d1a7465eb8a5f023b691c0308abb38a40.

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,26 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
 import Header from '@/components/Header.vue';
 import Input from '@/components/Input.vue';
 import Sidebar from '@/components/Sidebar.vue';
-
-const sidebarOpen = ref(true);
 </script>
 
 <template>
-  <div class="min-h-screen bg-[#090b10] text-white">
-    <Sidebar :open="sidebarOpen" @toggle="sidebarOpen = !sidebarOpen" />
+  <div class="flex w-full">
+    <Sidebar></Sidebar>
+    <main class="flex-1">
+      <Header></Header>
 
-    <main
-      class="relative min-h-screen flex flex-col justify-between transition-all duration-300"
-      :class="sidebarOpen ? 'lg:ml-64' : 'lg:ml-20'"
-    >
-      <div class="flex-1 px-4 pb-6 pt-4 sm:px-6 lg:px-8">
-        <Header />
-      </div>
-
-      <Input />
+      <div>Content area goes here</div>
+      <Input></Input>
     </main>
   </div>
 </template>
