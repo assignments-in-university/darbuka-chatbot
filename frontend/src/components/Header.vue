aa32eca298177db513b14c4405604be1b7894f12
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-07-14 15:18:44 +0800

    Implement header design.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import Gear from '@/assets/icons/Gear.vue';
+import Help from '@/assets/icons/Help.vue';
+
+</script>
+
 <template>
-  <header class="h-12 border-b w-full">
-    Header goes here
+  <header class="h-16 w-full bg-black text-white flex items-center px-4">
+    <h1 class="font-primary flex-1">Main Chatbot</h1>
+    <div class="flex gap-x-2 px-3 py-1 rounded-full border border-neutral-900 items-center mr-4">
+      <div class="rounded-full size-2 bg-emerald animate-pulse"></div>
+      <span class="text-sm text-neutral-400">Network Online</span>
+    </div>
+
+    <div class="flex items-center gap-x-4">
+      <Gear class="stroke-neutral-400 size-6"></Gear>
+      <Help class="fill-neutral-400 size-5"></Help>
+    </div>
   </header>
 </template>
