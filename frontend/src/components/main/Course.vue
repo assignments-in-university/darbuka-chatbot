30b72c54c2eae9c9144c00504348f7a95d0f6677
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-08-03 20:16:48 +0800

    Make the course page responsive.

--- frontend/src/components/main/Course.vue
+++ frontend/src/components/main/Course.vue
@@ -78,7 +78,7 @@ const generateCourse = async () => {
       :animate="{ opacity: 1, y: 0 }"
       class="w-full rounded-md border border-neutral-800 bg-neutral-900 p-10 relative overflow-hidden group hover:border-emerald duration-150 bg-[repeating-radial-gradient(var(--color-neutral-700)_0,var(--color-neutral-700)_1px,transparent_1px,transparent_100%)] bg-size-[20px_20px] mb-10 transition-colors"
     >
-      <div class="flex items-center gap-x-4 mb-4 font-primary">
+      <div class="flex flex-wrap sm:flex-nowrap items-center gap-4 mb-4 font-primary">
         <motion.span
           class="px-4 py-1 bg-emerald/10 border border-emerald rounded-md text-emerald"
           :initial="{ opacity: 0, x: -10 }"
@@ -87,15 +87,25 @@ const generateCourse = async () => {
         >
           {{ totalCourseDuration }} minutes of learning
         </motion.span>
-        <motion.span :initial="{ opacity: 0, x: -10 }" :animate="{ opacity: 1, x: 0 }" :transition="{ delay: 0.2 }">
+        <motion.span
+          class="hidden sm:block"
+          :initial="{ opacity: 0, x: -10 }"
+          :animate="{ opacity: 1, x: 0 }"
+          :transition="{ delay: 0.2 }"
+        >
           >
         </motion.span>
-        <motion.span :initial="{ opacity: 0, x: -10 }" :animate="{ opacity: 1, x: 0 }" :transition="{ delay: 0.3 }">
+        <motion.span
+          :initial="{ opacity: 0, x: -10 }"
+          :animate="{ opacity: 1, x: 0 }"
+          :transition="{ delay: 0.3 }"
+          class="italic"
+        >
           {{ course?.lessons.length }} Lessons
         </motion.span>
       </div>
       <motion.h2
-        class="font-primary lg:max-w-1/2 text-4xl font-medium mb-4 text-emerald"
+        class="font-primary lg:max-w-1/2 text-3xl sm:text-4xl font-medium mb-4 text-emerald"
         :initial="{ opacity: 0, y: 10 }"
         :animate="{ opacity: 1, y: 0 }"
         :transition="{ delay: 0.1 }"
@@ -116,11 +126,16 @@ const generateCourse = async () => {
       </div>
     </motion.div>
 
-    <div class="flex gap-x-4 px-10 max-w-3xl mx-auto">
-      <motion.div class="w-24" :initial="{ height: 0 }" :animate="{ height: 'auto' }" :transition="{ delay: 0.2, duration: 1 }">
+    <div class="flex gap-x-4 lg:px-10 md:max-w-3xl mx-auto">
+      <motion.div
+        class="w-24 hidden sm:block"
+        :initial="{ height: 0 }"
+        :animate="{ height: 'auto' }"
+        :transition="{ delay: 0.2, duration: 1 }"
+      >
         <div class="w-1 bg-neutral-700 rounded-full h-full mx-auto"></div>
       </motion.div>
-      <div class="w-full space-y-24 py-10">
+      <div class="w-full space-y-4 sm:space-y-24 py-10">
         <motion.div
           class="relative font-primary bg-neutral-900 p-4 rounded-md border border-neutral-800 cursor-pointer hover:border-emerald duration-150 transition-colors"
           :class="{ 'border-emerald!': currentLessonId === lesson.id }"
@@ -131,7 +146,29 @@ const generateCourse = async () => {
           v-for="(lesson, idx) in course?.lessons"
           :key="lesson.title + idx"
         >
-          <div class="flex gap-x-2 mb-2">
+          <div class="flex gap-2 mb-2 flex-wrap sm:flex-nowrap">
+            <span
+              class="px-3 py-0.5 text-emerald-500 bg-emerald-500/20 border border-emerald-500 rounded-md capitalize sm:hidden flex gap-x-2 items-center"
+              v-if="currentLessonId === lesson.id"
+            >
+              <div class="size-2 rounded-full bg-emerald-500"></div>
+              Current Lesson
+            </span>
+            <span
+              class="px-3 py-0.5 text-black bg-emerald-500 rounded-md capitalize sm:hidden flex gap-x-1 items-center"
+              v-if="idx < (course?.lessons?.findIndex((l) => l.id === currentLessonId) as number)"
+            >
+              <Tick class="size-5"></Tick>
+              Completed
+            </span>
+            <span
+              class="px-3 py-0.5 text-white bg-neutral-800 rounded-md capitalize sm:hidden flex gap-x-1 items-center border border-neutral-700"
+              v-else
+              v-if="idx > (course?.lessons?.findIndex((l) => l.id === currentLessonId) as number)"
+            >
+              <Lock class="stroke-white size-5"></Lock>
+              To Be Completed
+            </span>
             <span class="px-4 py-0.5 text-black bg-emerald rounded-md capitalize">
               {{ lesson.difficulty }}
             </span>
@@ -139,12 +176,12 @@ const generateCourse = async () => {
               {{ lesson.estimatedMinutes }} minutes
             </span>
           </div>
-          <h3 class="text-3xl font-semibold text-verdant mb-4">{{ lesson.title }}</h3>
+          <h3 class="text-2xl sm:text-3xl font-medium text-verdant mb-4">{{ lesson.title }}</h3>
           <p>{{ lesson.description }}</p>
 
           <!-- Is Completed Indicator -->
           <div
-            class="absolute size-16 border-2 border-emerald bg-emerald rounded-full top-1/2 -translate-y-1/2 left-0 -translate-x-[calc(100%+26px)] shadow-[0px_0px_20px_10px] shadow-verdant/10 flex items-center justify-center duration-150 transition-colors"
+            class="absolute size-12 md:size-16 border-2 border-emerald bg-emerald rounded-full top-1/2 -translate-y-1/2 left-0 -translate-x-[calc(100%+34px)] md:-translate-x-[calc(100%+26px)] shadow-[0px_0px_20px_10px] shadow-verdant/10 hidden sm:flex items-center justify-center duration-150 transition-colors"
             :class="{
               'border-emerald': currentLessonId === lesson.id,
               'bg-coal!': idx >= (course?.lessons?.findIndex((l) => l.id === currentLessonId) as number),
@@ -163,7 +200,7 @@ const generateCourse = async () => {
     </div>
 
     <button
-      class="w-full max-w-2xl font-primary bg-emerald text-black px-4 py-2 rounded-md mt-12 mx-auto font-medium cursor-pointer flex gap-x-2 justify-center items-center duration-150 transition-all"
+      class="w-full md:max-w-2xl font-primary bg-emerald text-black px-4 py-2 rounded-md mt-12 mx-auto font-medium cursor-pointer flex gap-x-2 justify-center items-center duration-150 transition-all"
       :class="{ 'bg-emerald/20! pointer-events-none': isLoading }"
       @click="generateCourse"
     >
